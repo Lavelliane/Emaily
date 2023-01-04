4759a60fc4b29af0b13152d5261f6af10e87f46b
Lavelliane <jhurylastre432@gmail.com>
2023-01-04 21:31:17 +0800

    Working on forms

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
+import validateEmail from "../../utils/validateEmail";
 
-const SurveyForm = ({ handleSubmit }) => {
+const FIELDS = [
+  {label: 'Survey Title', name: 'title'},
+  {label: 'Subject Line', name: 'subject'},
+  {label: 'Email Body', name: 'body'},
+  {label: 'Recipient List', name: 'emails'}
+]
 
-    const FIELDS = [
-        {label: 'Survey Title', name: 'title'},
-        {label: 'Subject Line', name: 'subject'},
-        {label: 'Email Body', name: 'body'},
-        {label: 'Recipient List', name: 'emails'}
-    ]
+const SurveyForm = ({ handleSubmit, onSurveySubmit }) => {  
 
   function renderFields(){
     return FIELDS.map(({label, name}, i) => {
@@ -17,15 +19,32 @@ const SurveyForm = ({ handleSubmit }) => {
     })
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit(values => console.log(values))}>
+    <div className="container">
+      <form onSubmit={handleSubmit(onSurveySubmit)}>
         {renderFields()}
-        <button type="submit">Submit</button>
+        <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
+        <button type="submit" className="teal btn-flat right white-text">
+          Next
+          <i className="material-icons right">done</i>
+        </button>
       </form>
     </div>
   );
 };
 
+function validate(values) {
+  const errors = {}
+  errors.emails = validateEmail(values.emails || '')
+  FIELDS.forEach(({name}) => {
+    if(!values[name]){
+      errors[name] = 'You must provide a value'
+    }
+  })
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "surveyForm",
+  destroyOnUnmount: false,
 })(SurveyForm);
--- server/client/src/components/surveys/SurveyNew.js
+++ server/client/src/components/surveys/SurveyNew.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 const SurveyNew = () => {
+  const [showFormReview, setShowFormReview] = useState(false)
+
+
+
   return (
     <div>
-        <SurveyForm />
+        {showFormReview === true && <SurveyFormReview onCancel={() => setShowFormReview(false)}/>}
+        {showFormReview === false && <SurveyForm onSurveySubmit={() => setShowFormReview(true)}/>}
     </div>
   )
 }
